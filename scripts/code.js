ee90f19b85e20fcbbdec152984833e175fa3f75d
wo-de-github000 <478618625@qq.com>
2023-02-22 15:24:03 +0800

    create3

--- scripts/code.js
+++ scripts/code.js
@@ -1,4 +1,3 @@
-
 /**
  *
  * (c) Copyright Ascensio System SIA 2020
@@ -16,59 +15,59 @@
  * limitations under the License.
  *
  */
+(function(window, undefined)
+{
+	var nStage   = 0;
+	//the text that will be searched in the document. 
+	var arrCodes = ["%FIRST_NAME%", "%LAST_NAME%", "%ADDRESS%"];
+	var arrIds   = ["inputFirstName", "inputLastName", "inputAddress"];
 
-// Example insert text into editors (different implementations)
-(function(window, undefined){
-    
-    var text = "Hello world!";
-	alert(0)
-    window.Asc.plugin.init = function()
-    {
-        var variant = 2;
+	function privateReplaceNext()
+	{
+		//properties structure for search and replace
+		var oProperties = {
+			"searchString"  : arrCodes[nStage],
+			"replaceString" : document.getElementById(arrIds[nStage]).value,
+			"matchCase"     : true
+		};
+		//execute method for search and replace
+		window.Asc.plugin.executeMethod("SearchAndReplace", [oProperties]);
 
-        switch (variant)
-        {
-            case 0:
-            {
-                // serialize command as text
-                var sScript = "var oDocument = Api.GetDocument();";
-                sScript += "oParagraph = Api.CreateParagraph();";
-                sScript += "oParagraph.AddText('Hello world!');";
-                sScript += "oDocument.InsertContent([oParagraph]);";
-                this.info.recalculate = true;
-                this.executeCommand("close", sScript);
-                break;
-            }
-            case 1:
-            {
-                // call command without external variables
-                this.callCommand(function() {
-                    var oDocument = Api.GetDocument();
-                    var oParagraph = Api.CreateParagraph();
-                    oParagraph.AddText("Hello world!");
-                    oDocument.InsertContent([oParagraph]);
-                }, true);
-                break;
-            }
-            case 2:
-            {
-                // call command with external variables
-                Asc.scope.text = text; // export variable to plugin scope
-                this.callCommand(function() {
-                    var oDocument = Api.GetDocument();
-                    var oParagraph = Api.CreateParagraph();
-                    oParagraph.AddText(Asc.scope.text); // or oParagraph.AddText(scope.text);
-                    oDocument.InsertContent([oParagraph]);
-                }, true);
-                break;
-            }
-            default:
-                break;
-        }
-    };
+		nStage++;
+		if (nStage >= arrCodes.length)
+			window.Asc.plugin.executeCommand("close", "");
+	}
 
-    window.Asc.plugin.button = function(id)
-    {
-    };
+	window.Asc.plugin.init = function()
+	{
+		var oThis = this;
+		document.getElementById("buttonCancel").onclick = function()
+		{
+			this.executeCommand("close", "");
+		};
+
+		document.getElementById("buttonOK").onclick = function()
+		{
+			privateReplaceNext();
+		};
+	};
+
+	window.Asc.plugin.onMethodReturn = function(returnValue)
+	{
+		//event return for completed method
+		var _plugin = window.Asc.plugin;
+		if (_plugin.info.methodName == "SearchAndReplace")
+		{
+			privateReplaceNext();
+		}
+	};
+
+	window.Asc.plugin.button = function(id)
+	{
+		if (-1 === id)
+		{
+			this.executeCommand("close", "");
+		}
+	};
 
 })(window, undefined);
